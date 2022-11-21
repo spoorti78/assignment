cfdfb3e7fcf223b10d11ef468ede1e477f2382a5
spoorti78 <117076715+spoorti78@users.noreply.github.com>
2022-11-21 22:48:00 +0530

    assignment_q2

---
+++ _a2_sort1.py
@@ -0,0 +1,8 @@
+dic={}
+alpha=['a','b','c','d','e','f','g','i','j','l','m','n','p','q','r','s','t','u','v','w','x','y','z']
+for i in alpha:
+    asc=ord(i)
+    key=i
+    value=asc
+    dic[key]=value
+print(dic)
