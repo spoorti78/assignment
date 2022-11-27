f79fcd2d4d36adb0a616a198bc2684d3509ce026
spoorti78 <117076715+spoorti78@users.noreply.github.com>
2022-11-27 20:19:40 +0530

    assignment3_q3

---
+++ a3_q31.py
@@ -0,0 +1,16 @@
+# program to find number of uppercase and lowercase letter.
+# input is taken from user.
+x=input("Enter the string:- ")
+def char(x):
+  u=0
+  l=0
+  for i in x:
+      if i>='a' and i<='z':
+       l+=1
+
+      if i >='A' and i<='Z':
+       u+=1
+
+  print("LowerCase letter in the String",l)
+  print("UpperCase letter in the String",u)
+char(x)
